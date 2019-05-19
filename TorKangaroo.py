1af5e73d3cd77ff75e4f063a1d7d44f6de465911
Blastpp <kigeule123@gmail.com>
2019-05-19 16:44:36 -0400

    Add files via upload
    Add Keyboard Interrupt :
        - Turning off Tor ghost
        - Showing your Public ip at the end to Confirm all work correctly

---
+++ TorKangaroo.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import time
+import signal 
+import threading
+import requests
+
+#From Section
+
+from termcolor import colored, cprint
+
+# Script Starting
+
+
+
+#trying to add KeyboardInterrupt
+
+def sigint_handler(signal, frame):
+    print('   You pressed Ctrl+C!')
+    time.sleep(3)
+    print('Turn off TorGhost...')
+    time.sleep(3)
+    os.system('torghost stop')
+    os.system('clear')
+    print('And you are back on your public IP:  ')
+    print(requests.get('http://ip.42.pl/raw').text)
+    sys.exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
+
+#Does TorGhost path exist?
+dirExist = os.path.exists('./torghost')
+if dirExist == False:   #If not, Downloading and Installing it.
+    os.system('git clone https://github.com/susmithHCK/torghost.git')
+    os.system('cd torghost/')
+    os.system('chmod +x torghost/install.sh')
+    os.system('torghost/./install.sh')
+    os.system('clear')
+
+    # Setting Timer Value (In second)
+    userTimer = int(input('Enter timer between ip change :  ')) 
+    t = userTimer
+
+    # Starting The Engine..
+    print('Starting Tor Bouncing')
+    time.sleep(3)
+    os.system('torghost start')
+    while(True):
+        os.system('torghost switch')
+        time.sleep(t)
+
+# If TorGhost Directory exist, Just start the engine.
+
+if dirExist == True:
+
+
+    userTimer = int(input('Enter timer between ip change :  '))
+    t = userTimer
+
+    print('Starting Tor Bouncing')
+    time.sleep(3)
+
+    os.system('torghost start')
+
+    while(True):
+        os.system('torghost switch')
+        time.sleep(t)
+
